0d856a3ff3423359ff887028208a1fc1333e2e54
lleandromelo <leandro.mfarias@hotmail.com>
2022-04-11 21:49:14 -0300

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,6 @@
 function Home(){
-    return <h2>Acesse o site CMA Natal (Estamos em construção) 
-<a href="https://www.cmanatal.com./">E venha se tornar um Piloto de Avião ou Comissário de Voo</a></h2>
+    return <h2>Em Natal, primeiro espaço médico para obtenção de Certificado Médico Aeronáutico de 2ª, 4ª e 5ª Classes para Pilotos e Comissários (84 98129 1987) 
+<a href="https://www.cmanatal.com./">Acesse o site CMA Natal</a></h2>
 }
 
 export default Home
