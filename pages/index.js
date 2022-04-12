a90b69b9bd125c2e5bdc8b347cfbd8dc24afa826
lleandromelo <leandro.mfarias@hotmail.com>
2022-04-11 21:44:27 -0300

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,6 @@
 function Home(){
-    return </h2>
-    <h2><p>Acesse o site CMA Natal (Estamos em construção) 
-<a href="https://www.cmanatal.com./">E venha se tornar um Piloto de Avião ou Comissário de Voo</a></p></h2>
+    return <h2>Acesse o site CMA Natal (Estamos em construção) 
+<a href="https://www.cmanatal.com./">E venha se tornar um Piloto de Avião ou Comissário de Voo</a></h2>
 }
 
 export default Home
